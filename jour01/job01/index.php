6ba8f7efc1b4e8ee562a6559810132ca51e45cae
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-05-04 10:51:09 +0200

    job01 fini modifié

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -3,9 +3,11 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style1.css">
-    <link rel="stylesheet" href="style2.css">
-    <link rel="stylesheet" href="style3.css">
-    <link rel="stylesheet" href="style4.css">
+    <link rel="stylesheet" href="style2.css" media="screen and (min-width: 1680px) and (max-width:1920px)">
+    <link rel="stylesheet" href="style3.css" media="screen and (min-width: 1280px) and (max-width:1680px)">
+    <link rel="stylesheet" href="style4.css" media="screen and (max-width:1280px)">
+
+
     <title></title>
   </head>
   <body>
