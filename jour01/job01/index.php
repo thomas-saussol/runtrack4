57d2bff404e857b78ba0aaccef7e45c5d0b58f6d
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-05-04 09:44:28 +0200

    test

---
+++ jour01/job01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="style4.css">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
