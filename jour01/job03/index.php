79e39866c94090fa8046fb2efc64934a6106fc8f
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-05-04 11:56:35 +0200

    job03 fini

---
+++ jour01/job03/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+
+<section class="section">
+
+</section >
+
+<section class="section">
+
+</section>
+
+<section class="section">
+
+</section>
+
+<section class="section">
+
+</section>
+
+  </body>
+</html>
