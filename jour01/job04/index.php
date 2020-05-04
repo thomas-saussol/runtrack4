5ed80269642bca2038c2856fb0b35e773d763889
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-05-04 13:02:01 +0200

    job04 fini

---
+++ jour01/job04/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <title></title>
+  </head>
+
+    </body>
+
+    <nav role="navigation">
+      <div id="menuToggle">
+
+        <input type="checkbox"  id='checkbox'/>
+
+
+        <span></span>
+        <span></span>
+        <span></span>
+
+
+        <ul id="menu">
+          <a href="#"><li>Accueil</li></a>
+          <a href="#"><li>Présentation</li></a>
+          <a href="#"><li>Contact</li></a>
+          </ul>
+      </div>
+    </nav>
+
+  </body>
+</html>
