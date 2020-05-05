bc8cc5a4b7652809021386ecf36221ee53085d21
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-05-05 11:44:25 +0200

    modification finies

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -2,7 +2,7 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>job01</title>
+    <title>job02</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
@@ -96,32 +96,16 @@
 
 </form>
 
+<footer>
+  <ul >
+    <li><a href="index.php">Lien 1</a></li>
+    <li><a href="index.php">Lien 2</a></li>
+    <li><a href="index.php">Lien 3</a></li>
+    <li><a href="index.php">Lien 4</a></li>
+  </ul>
 
-<footer class="page-footer">
-     <div class="container">
-       <div class="row">
-         <div class="col l6 s12">
-           <h5 class="white-text">Breef</h5>
-           <p class="grey-text text-lighten-4">Amusez-vous bien!</p>
-         </div>
-         <div class="col l4 offset-l2 s12">
-           <h5 class="white-text">Liens</h5>
-           <ul>
-             <li><a class="col s12 m6 l3 white" href="index.php">Link 1</a></li>
-             <li><a class="col s12 m6 l3 white" href="index.php">Link 2</a></li>
-             <li><a class="col s12 m6 l3 white" href="index.php">Link 3</a></li>
-             <li><a class="col s12 m6 l3 white" href="index.php">Link 4</a></li>
-           </ul>
-         </div>
-       </div>
-     </div>
-     <div class="footer-copyright">
-       <div class="container">
-       © 2020 Thomas's Company
-       <a class="grey-text text-lighten-4 right" href="#!">Encore plus d'infos!</a>
-       </div>
-     </div>
-   </footer>
+</footer>
+
 
   </body>
 </html>
