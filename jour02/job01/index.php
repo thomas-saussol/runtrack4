6a7665df4593446f02f8fe9fc1110fe3badd51d2
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-05-05 10:20:57 +0200

    Jour 02 job 01

---
+++ jour02/job01/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>job01</title>
+  </head>
+  <body>
+<header>
+  <nav>
+    <ul >
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="index.php">Inscription</a></li>
+      <li><a href="index.php">Connexion</a></li>
+      <li><a href="index.php">Rechercher</a></li>
+    </ul>
+  </nav>
+</header>
+
+<form class="" action="index.html" method="post">
+  <input type="radios" name="" value="" placeholder="Civilité">
+  <input type="text" name="Nom" value="" placeholder="Nom">
+  <input type="text" name="Prénom" value="" placeholder="Prénom">
+  <input type="text" name="Adresse" value="" placeholder="Adresse">
+  <input type="email" name="Email" value="" placeholder="Email">
+  <input type="password" name="Password" value="" placeholder="Password">
+  <p>Les Passions:</p>
+
+
+  <input type="checkbox" id="scales" name="scales"
+         checked>
+  <label for="scales">Informatique</label>
+
+
+
+  <input type="checkbox" id="horns" name="horns">
+  <label for="horns">Voyage</label>
+
+
+
+  <input type="checkbox" id="horns" name="horns">
+  <label for="horns">Sport</label>
+
+
+
+  <input type="checkbox" id="horns" name="horns">
+  <label for="horns">Lecture</label>
+
+
+</form>
+
+
+
+<footer>
+  <ul >
+    <li><a href="index.php">Lien 1</a></li>
+    <li><a href="index.php">Lien 2</a></li>
+    <li><a href="index.php">Lien 3</a></li>
+    <li><a href="index.php">Lien 4</a></li>
+  </ul>
+
+</footer>
+
+  </body>
+</html>
