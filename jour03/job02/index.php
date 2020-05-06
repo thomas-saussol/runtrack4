32817305ba7d4d42b683a941f5291f2c345b60e1
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-05-06 20:44:49 +0200

    job 02 pas totalement finit, réussit à rediriger vers le site, réussi a faire apparaitre le modal depuis le bouton, a empêcher les envoies de formulaire si mdp et email vide sauf n'arrive pas à changer les couleurs

---
+++ jour03/job02/index.php
@@ -0,0 +1,205 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<style media="screen">
+
+body{
+background:#dcdcdc;
+}
+
+.row{
+  margin:10px;
+}
+
+.jumbotronn{
+background-color: rgba(247, 247, 247, 0.82);
+  padding: 15px;
+}
+
+</style>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">LPTF</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="https://laplateforme.io/">Accueil <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Units</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Jobs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Skills</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="text-center">
+
+      <h1>LaPlateforme_</h1>
+    </div>
+<div class="d-flex justify-content-around">
+  <div class="w-3 p-3" style="Height:34rem">
+    <img src="img/1.jpg" class="card-img-top" alt="...">
+    <div class="card-body" style="background:White;">
+      <h5 class="card-title">Un Papillon</h5>
+      <p class="card-text">Un papillon, c'est un peu comme une chenille, mais avec des ailes.
+      <br>Ne pas ingerer</p>
+
+      <button type="button" data-toggle="modal" data-target="#infos" class="btn btn-primary">Commander votre propre papillon</button>
+      <div class="modal" id="infos">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Bravo vous avez acheté votre premier papillon!</h4>
+            </div>
+            <div class="modal-body">
+              Prenez en soin et donnez lui un prénom!
+            </div>
+            <div class="modal-footer">
+              <em>Réception du colis d'ici 3 semaines</em>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="jumbotronn" >
+  <h1 class="display-4">Bonjour, monde!</h1>
+  <p class="lead">Il existe plusieurs versions du terme: <br><br>le monde est la matière, l'espace le phénomène qui noussont accessibles par les sens, l'expérience ou la raison. <br><br> Le sens le plus courant désigne notre planète, la Terre, avec ses habitants, et son environnement plus ou moins naturel.</p>
+  <hr class="my-4">
+  <p>Le sens étendu designe l'univers dans son ensemble.</p>
+  <a class="btn btn-primary btn-lg .text-danger" href="#" role="button" >Rebooter le Monde</a>
+  <div class="spinner-border" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+
+  <div class="row justify-content-end" role="toolbar" aria-label="Toolbar with button groups">
+    <div class="btn-group mr-2" role="group" aria-label="First group">
+      <button type="button" class="btn btn-secondary"><< </button>
+      <button type="button" class="btn btn-secondary">1</button>
+      <button type="button" class="btn btn-secondary">2</button>
+      <button type="button" class="btn btn-secondary">3</button>
+      <button type="button" class="btn btn-secondary">>></button>
+    </div>
+</div>
+
+
+  </div>
+
+
+<div class="w-60 p-3">
+  <ul>
+    <li class="list-group-item active">Limbes</li>
+    <li class="list-group-item">Luxure</li>
+    <li class="list-group-item">Gourmandise</li>
+    <li class="list-group-item">Avarice</li>
+    <li class="list-group-item">Colere</li>
+    <li class="list-group-item">Heresie</li>
+    <li class="list-group-item">Violence</li>
+    <li class="list-group-item">ColereRuse et Tromperie</li>
+    <li class="list-group-item">Trahison</li>
+    <li class="list-group-item">Internet Explorer</li>
+  </ul>
+
+</div>
+
+  </div>
+<div class="container">
+  <!-- progress barre -->
+  <div class="row justify-content-end">
+
+    <h3>Installation de AI 9000</h3>
+  </div>
+  <div class="row justify-content-end">
+
+  </div>
+  <div class="progress">
+    <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
+
+</div>
+
+<div class="container row ">
+<div class="row">
+  <div class="col">
+
+
+  <!-- formulaire -->
+<h5>Recevez votre copie gratuite d'internet 2!</h5>
+  <div class="input-group mb-">
+    <div class="input-group-prepend">
+      <span class="input-group-text" id="basic-addon1">@</span>
+    </div>
+    <input type="text" class="form-control" placeholder="Login" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Mot de Passe" aria-label="Recipient's username" aria-describedby="basic-addon2">
+    <div class="input-group-append">
+      <span class="input-group-text" id="basic-addon2">@example.com</span>
+    </div>
+  </div>
+
+  <label for="basic-url">Your vanity URL</label>
+
+  <div class="input-group mb-3">
+    <div class="input-group-prepend">
+      <span class="input-group-text">DogeCoin</span>
+    </div>
+    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+    <div class="input-group-append">
+      <span class="input-group-text">.00</span>
+    </div>
+  </div>
+
+  <div class="input-group">
+    <div class="input-group-prepend">
+      <span class="input-group-text">https://l33t.lptf/dkwb/berlusconimkt/</span>
+    </div>
+    <textarea class="form-control" aria-label="With textarea"></textarea>
+  </div>
+</div>
+</div>
+
+  <form>
+    <div class="row justify-content-">
+      <label for="exampleInputEmail1" >Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1" >Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" required>
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+
+  </body>
+</html>
